98d8267afbf703cea58172c058107e3d9ccbed6b
FishoFire <rowandekeijser@hotmail.com>
2022-08-01 12:36:58 +0200

    kami hanamura added

--- test-maps/togglescript.js
+++ test-maps/togglescript.js
@@ -7,14 +7,16 @@ if (x == 0){
     1: "test-maps/krdata.opy",
     2: "test-maps/hollydata.opy",
     3: "test-maps/route.opy",
-    4: "test-maps/minimap.opy"
+    4: "test-maps/minimap.opy",
+    5: "test-maps/hanamura.opy"
     }[x]
 
     selectedmap = {
     1: "kingsRow",
     2: "hollywood",
     3: "route66",
-    4: "workshopChamber"
+    4: "workshopChamber",
+    5: "hanamura"
     }[x]
 
     x = '#!define editorordata #!include "'+ datafiles + '"\n #!define selectedmap "' + selectedmap + '"' + '\n #!define editoron false'
