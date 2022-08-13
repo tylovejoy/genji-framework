4fdf60794e7ba77ce371bd1008902575754672ca
FishoFire <rowandekeijser@hotmail.com>
2022-08-13 14:35:34 +0200

    added cp 1 bug map and notes for it

--- test-maps/togglescript.js
+++ test-maps/togglescript.js
@@ -10,7 +10,8 @@ if (x == 0){
     4: "test-maps/minimap.opy", // really small workshop map to test leaderboard
     5: "test-maps/hanamura.opy", // 100 cp hana kami
     6: "test-maps/busan.opy",// super short busan to test portals
-    7: "test-maps/testinghana.opy" // from BDRQ4 the test map they build us
+    7: "test-maps/testinghana.opy", // from BDRQ4 the test map they build us
+    8: "test-maps/cp1skipbug.opy" // the map data that sometimes skips cp 1 on reseting when people spec you
     }[x]
 
     selectedmap = {
@@ -20,7 +21,8 @@ if (x == 0){
     4: "workshopChamber",
     5: "hanamura",
     6: "busan",
-    7: "hanamura"
+    7: "hanamura",
+    8: "hanamura"
     }[x]
 
     x = '#!define editorordata #!include "'+ datafiles + '"\n #!define selectedmap "' + selectedmap + '"' + '\n #!define editoron false'
