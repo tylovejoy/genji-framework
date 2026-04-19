cd2616fab5a6f35a9c3c1f7bc90528727b666994
LulledLion:LocalUser <LocalUser@Example.com>
2026-04-18 21:00:42 -0500

    Hero adjustable Discord link

--- FwHero/fwHero.js
+++ FwHero/fwHero.js
@@ -4,13 +4,20 @@ if (Array.isArray(Hero)){
     Result = Hero.map(i => '#!include "FwHero/' + i).join('.opy"\n');
 }
 else{
+    Discord = {
+        "Doomfist": "dsc.gg/projectmomentum",
+        "Genji": "dsc.gg/genjiparkour",
+        "Kiriko": "",
+        "Lucio": "discord.com/invite/G9QBCDY",
+        "Mercy": "discord.gg/mercyparkour",
+    }
     Result =
 'enum FwHero:\n' +
     '\tHero = Hero.' + Hero.toUpperCase() + '\n' +
     '\tString = "' + Hero + '"\n' +
     '\tStringLC = "' + Hero.toLowerCase() + '"\n' +
     '\tStringUC = "' + Hero.toUpperCase() + '"\n' +
-//'#!define FwHero@Hero ' + Hero.toLowerCase() + '\n' +
+    '\tDiscord = "' + Discord[Hero] + '"\n' +
 '#!define Fw_Include_Hero #!include "FwHero/' + Hero + '.opy"\n';
 }
 Hero = Result
